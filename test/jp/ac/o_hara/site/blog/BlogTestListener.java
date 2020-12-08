3f753062fd5f617adef282cd9fd4813d1c613aae
kikuchi <kikuchi@macbookair-22a6.tokyo.o-hara>
2020-12-08 15:03:06 +0900

    ブログ仮実装

--- test/jp/ac/o_hara/site/blog/BlogTestListener.java
+++ test/jp/ac/o_hara/site/blog/BlogTestListener.java
@@ -1,35 +1,37 @@
 package jp.ac.o_hara.site.blog;
 
+import java.sql.Date;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-/**
- * Application Lifecycle Listener implementation class BlogTestListener
- *
- */
 @WebListener
 public class BlogTestListener implements ServletContextListener {
+	BlogDAO dao = null;
 
-    /**
-     * Default constructor. 
-     */
-    public BlogTestListener() {
-        // TODO Auto-generated constructor stub
-    }
+	public BlogTestListener() {
+		dao = BlogDAO.getInstanse();
+	}
 
-	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		if (System.getenv("DATABASE_URL") != null) {
+		} else {
+			if(dao.execSQL("CREATE TABLE IF NOT EXISTS blogtbl"
+					+ " (id IDENTITY , userID VARCHAR(64), date DATE , title VARCHAR(64), article VARCHAR(256))")) {
+				System.out.println("TestBlogDB is READY.");
+			} else {
+				System.out.println("TestUserDB is NOT READY.");
+			}
+		}
+		dao.create(new BlogBean("hoge", Date.valueOf("2020-12-08"), "ほげ", "ほげほげ"));
+		dao.create(new BlogBean("piyo", Date.valueOf("2020-12-07"), "ぴよ", "ぴよぴよ"));
+		//4つテスト用に作る
+	}
 
-	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
-	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		dao = null;
+	}
 }
